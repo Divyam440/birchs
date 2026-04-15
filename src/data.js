11623675f161fa018ad1f8442b7b6683520921db
Divyam <divyamjoshi2004@gmail.com>
2026-04-14 18:06:28 -0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,123 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const firstHeading = "A Introvert was born today, 22 years ago!"
+// Main heading text - Change this text as you like
+
+export const firstSubtext = "So I Have A little surprise That awaits...For Her❤️"
+// Subtext under heading (optional) - Change, leave blank or remove.
+
+
+// ==========================
+// 2. Cake Screen
+// ==========================
+// Add recipient's name here.
+export const NAME = "Introvert!"
+
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Some Sweet Moments"
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.jpeg" }, Match the format and add more as needed
+export const photos = [
+    "/images/1.jpg",
+    "/images/2.jpg",
+    "/images/3.jpg",
+    "/images/4.jpg",
+    "/images/5.jpg",
+    "/images/6.jpg",
+    "/images/7.jpg",
+    "/images/8.jpg",
+    "/images/9.jpg",
+    "/images/10.jpg",
+    "/images/11.jpg",
+    "/images/12.jpg",
+    "/images/13.jpg",
+    "/images/14.jpg",
+    "/images/15.jpg",
+    "/images/16.jpeg",
+]
+
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "A Special Message"
+// Heading above card - Change this text as you like
+
+export const specialMessage = `So hey…
+I tried a lot to think how to start this… but maybe the truth is, I never really knew how to start anything with you as always aj bhi fumble kr jata hu
+
+It’s been almost a year now  and life has changed a lot.
+Different routine, different people,different priorities…
+but somehow,some things just stayed exactly where they were.
+Like Earlier we used to meet every day, talk about everything,laugh without any reason…
+and now it’s like 2–3 months mein ek baar baat hoti hai.
+And I act like it’s normal… but honestly,.
+
+Tera message notification mein dekhna aur reply ka wait karna…
+phir wo wait ka dheere dheere disappointment mein change hona…
+and finally message delete kar dena
+shayad ye chhoti si cheez lagti hai,but it says more than I ever said.and believe nhi hota So You Can check last 30 messages bet ispr toh koi justification bhi nhi de skti ok🤣🤣mazak Kr Rha Yar Birthday Pr thoda Sa Toh Chlta h ab birthday h Toh Haan,thoda sa complain hai…but not a big deal Now
+cause the truth is…
+Back then you were never just “someone” to me.
+
+Teri jo simple,joyfull nature tha or hai.That pure,soft, understanding soul…
+I didn’t just admire it… I Just have got attached to it.
+Maybe more than I should have at that time.
+
+Aur shayad isi liye… jab tu hurt hui meri wajah se…That scribble day and many more Things now If i Think back joh bhi har 2 din m ladai hoti this wo sirf ek mistake nahi thi mere liye.
+It stayed. It still stays.
+The  message… the quote... the bad-trip memories I remember it more than I admit.
+
+And maybe isi liye…
+I made these two websites, did all that mehnat.
+Logically it doesn’t make sense… but feelings bond kab koi logic follow karte  hain.
+And
+You don’t owe me anything.
+Sach bolu toh… main hi shayad thoda sa reh gaya tha wahan.
+
+That’s why I asked about Node and Next.js…
+shayad bas ek reason chahiye tha To minimize those guilt which in this life I can't make it right only minimize it but if I ever get a chance again I'll just Mtlb rone dena  bhi dur ki baat h never even make you hurt or make you sad in anyway.
+
+And yeah… that last photo… your smile…
+I don’t know why, but it never left.
+Some things just don’t leave… no matter how much time passes.
+
+I don’t know what this is called…
+maybe Bond, maybe care, maybe something Else…
+But jo bhi tha hai… it was pure real.
+
+So today, on your birthday…
+I just want to say
+Happy Birthday Introvert ✨
+
+So...Stay happy… stay that same pure soul…
+because people like you don’t come twice in someone’s life.
+
+And yeah…if I ever meant even a little to you…just don’t forget me completely. 💖
+
+Because no matter how much life changes… some people don’t leave your life they just stop being part of your Good days,Just become Memories And always Live Purely In Pure hearts. 💗`
+// Main special message (you can edit freely)
+
+
+// ==========================
+// 4. Final Screen
+// ==========================
+export const finalScreenHeading = "One Last Thing..."
+// Heading above final surprise - Change this text as you like
+
+export const overlayText = "Hope You Loved Your Gift❤️All the Best for your future"
+// First text inside the overlay card - Change this text as you like
+
+export const overlayMessage = "Once again, Happy Birthday Introvert! Hope you loved your surprise."
+
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3"
